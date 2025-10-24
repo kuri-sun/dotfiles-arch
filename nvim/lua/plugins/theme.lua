87dd8d4d97eb455d8b622b561c65fe291135536e
kuri-sun <harukikuriwada@gmail.com>
2025-10-24 22:04:53 +0900

    chore: remove the transparency for nvim

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,13 +1,13 @@
 return {
 	{
-		-- "kuri-sun/yoda.nvim",
-		dir = vim.fn.stdpath("config") .. "/lua/yoda.nvim",
+		"kuri-sun/yoda.nvim",
+		-- dir = vim.fn.stdpath("config") .. "/lua/yoda.nvim",
 		name = "yoda",
 		lazy = false,
 		priority = 100,
 		opts = {
 			theme = "dark",
-			transparent_background = true,
+			-- transparent_background = true,
 		},
 	},
 	{
